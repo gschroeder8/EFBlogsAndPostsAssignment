cbafa79973296c6e6f4d83bd808819b517f08b46
Gavin Schroeder <gschroeder8@my.wctc.edu>
2024-04-15 23:21:18 -0500

    Setting up switch statement and user menu looping control

--- Program.cs
+++ Program.cs
@@ -1,37 +1,38 @@
-﻿using NLog;
+﻿using System;
 using System.Linq;
+using NLog;
 
-// See https://aka.ms/new-console-template for more information
-string path = Directory.GetCurrentDirectory() + "//nlog.config";
+var db = new BloggingContext();
 
-// create instance of Logger
-var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
-logger.Info("Program started");
-
-try
+while (true)
 {
-    // Create and save a new Blog
-    Console.Write("Enter a name for a new Blog: ");
-    var name = Console.ReadLine();
+    Console.WriteLine("1. Display all blogs");
+    Console.WriteLine("2. Add Blog");
+    Console.WriteLine("3. Create Post");
+    Console.WriteLine("4. Display Posts");
+    Console.WriteLine("Enter your option:");
 
-    var blog = new Blog { Name = name };
+    var userOption = Console.ReadLine();
 
-    var db = new BloggingContext();
-    db.AddBlog(blog);
-    logger.Info("Blog added - {name}", name);
-
-    // Display all Blogs from the database
-    var query = db.Blogs.OrderBy(b => b.Name);
-
-    Console.WriteLine("All blogs in the database:");
-    foreach (var item in query)
+    try
     {
-        Console.WriteLine(item.Name);
+        switch (userOption)
+        {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "3":
+                break;
+            case "4":
+                break;
+            default:
+                Console.WriteLine("Invalid option");
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"An error occurred: {ex.Message}");
     }
 }
-catch (Exception ex)
-{
-    logger.Error(ex.Message);
-}
-
-logger.Info("Program ended");
